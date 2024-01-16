374549f0ba72c753f127424f8f6b4366862c1930
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-17 00:01:18 +0530

    Reading Writing file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 const fs = require("fs");
 
-fs.writeFileSync("read.txt","Hello world");
+// fs.writeFileSync("read.txt","Hello world");
 
-fs.writeFileSync("read.txt","Hello world again");
+// fs.writeFileSync("read.txt","Hello world again");
+
+// fs.appendFileSync("read.txt","I am 22 years old ");
+
+// const buffData = fs.readFileSync("read.txt");
+// console.log(buffData);
+
+// const orgData = buffData.toString();
+// console.log(orgData);
+
+//?to rename the file
+fs.renameSync("read.txt","readwrite.txt");
