c1644d36ae85a77f9b1df798a7df6f9d7924505a
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-19 16:47:15 +0530

    Routing in Node.js

--- HTTP-Server/index.js
+++ HTTP-Server/index.js
@@ -3,7 +3,18 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.end("This message from server side");
+    // console.log(req.url);
+    if(req.url=="/"){
+       res.end("Home Page"); 
+    }else if(req.url=="/about"){
+        res.end("About Page");
+    }else if(req.url=="/contact"){
+        res.end("Contact Page");
+    }else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end("<h1>404 Error Page</h1>");
+    }
+    
 })
 
 server.listen(8000,"127.0.0.1",()=>{
