300bf51fbe71f54ae6e813504d4242d4c83d536d
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-19 18:44:19 +0530

    Api Creation

--- HTTP-Server/index.js
+++ HTTP-Server/index.js
@@ -1,22 +1,23 @@
+const http = require("http");
+const fs = require("fs");
 
+const server = http.createServer((req, res) => {
+  const data = fs.readFileSync(`${__dirname}/ApiData/data.json`, "utf-8");
+  let obj = JSON.parse(data);
 
-const http = require('http');
+  if (req.url == "/") {
+    res.end("Home Page");
+  } else if (req.url == "/about") {
+    res.end("About Page");
+  } else if (req.url == "/userapi") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(obj.users[1].name);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>404 Error Page</h1>");
+  }
+});
 
-const server = http.createServer((req,res)=>{
-    // console.log(req.url);
-    if(req.url=="/"){
-       res.end("Home Page"); 
-    }else if(req.url=="/about"){
-        res.end("About Page");
-    }else if(req.url=="/contact"){
-        res.end("Contact Page");
-    }else{
-        res.writeHead(404,{"Content-type":"text/html"});
-        res.end("<h1>404 Error Page</h1>");
-    }
-    
-})
-
-server.listen(8000,"127.0.0.1",()=>{
-    console.log("Listening");
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening");
+});
