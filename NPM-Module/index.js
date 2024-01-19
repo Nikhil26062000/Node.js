a67322fafcfcf79afcf05b1000ca421229051804
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-19 15:46:31 +0530

    Working in Chalk module

--- NPM-Module/index.js
+++ NPM-Module/index.js
@@ -3,6 +3,6 @@ const chalk = require("chalk");
 const validator = require("validator");
 
 console.log(chalk.blue("Hello World"));
-const result = validator.isEmail("Nikhilgmail.com");
+const result = validator.isEmail("Nikhil@gmail.com");
 
-console.log(result ? chalk.green.inverse(result):chalk.red.inverse(result));
+console.log(result ? chalk.green.inverse(result):chalk.red.inverse(result)); 
