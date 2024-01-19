a819e7b0cc58cfd964fadf7b112c9279669e5b5b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-19 16:05:16 +0530

    ModuleWrapperFunction

---
+++ ModulewrapperFunction/index.js
@@ -0,0 +1,10 @@
+
+//? module wrapper function
+// ( function(exports , require, module, __filename, __dirname ){
+
+// } )
+
+const name = "Nikhil Raj";
+console.log(name);
+console.log(__filename);
+console.log(__dirname);
