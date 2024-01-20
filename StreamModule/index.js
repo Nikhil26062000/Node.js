7cb750f3b741e5dc71aece99675254c25c1ed5d7
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-20 19:44:09 +0530

    StreamModule

---
+++ StreamModule/index.js
@@ -0,0 +1,35 @@
+
+
+var fs = require('fs');
+const http = require('http');
+
+const server = http.createServer();
+
+server.on("request",(req,res)=>{
+    
+    //*this is reading data using readFile
+    // fs.readFile("read.txt",(err,data)=>{
+    //     if(err) return console.error(err);
+    //     res.end(data.toString());
+    // })
+
+    //? Now reading data using Straem
+    const rstream = fs.createReadStream("read.txt");
+
+    rstream.on("data",(chunkdata)=>{
+        res.write(chunkdata);
+    })
+
+    rstream.on("end",()=>{
+        res.end();
+    })
+
+    rstream.on("error",(err)=>{
+        console.error(err);
+        res.end(err.message);
+    })
+})
+
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("listening");
+})
