e6b3844c9ef4a27e428beb334bea2ceb089ea507
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-21 23:26:24 +0530

    Handlebars , hbs , views , changing the name of views directory

--- Expressjs/src/index.js
+++ Expressjs/src/index.js
@@ -5,11 +5,28 @@ const express = require('express');
 
 const app = express();
 
-console.log(path.join(__dirname, '../public'));
-const staticPath = path.join(__dirname, '../public')
+//console.log(path.join(__dirname, '../public'));
+//const staticPath = path.join(__dirname, '../public')
+const templatesPath = path.join(__dirname, '../templates');
+
+
+//to set the view engine
+app.set('view engine', 'hbs');
+app.set('views',templatesPath);
+
 
 //! Using Built-in Middleware to use static file
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
+
+//template engine route
+app.get('/',(req, res)=>{
+    res.render('index');
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about');
+})
+
 
 app.get("/",(req,res) => {
     res.send("Hello World!");
