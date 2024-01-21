cb25ff8f23e850dbfbba3a2c754d53666f8cb1e6
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-22 00:00:25 +0530

    Partials in handlebars

--- Expressjs/src/index.js
+++ Expressjs/src/index.js
@@ -2,17 +2,21 @@
 const path = require('path');
 
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 //console.log(path.join(__dirname, '../public'));
 //const staticPath = path.join(__dirname, '../public')
-const templatesPath = path.join(__dirname, '../templates');
+const templatesPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 
 //to set the view engine
 app.set('view engine', 'hbs');
 app.set('views',templatesPath);
+hbs.registerPartials(partialsPath);
+
 
 
 //! Using Built-in Middleware to use static file
@@ -28,13 +32,13 @@ app.get('/about',(req, res)=>{
 })
 
 
-app.get("/",(req,res) => {
-    res.send("Hello World!");
-})
+// app.get("/",(req,res) => {
+//     res.send("Hello World!");
+// })
 
-app.get("/about",(req,res) => {
-    res.send("Welcome to about page");
-})
+// app.get("/about",(req,res) => {
+//     res.send("Welcome to about page");
+// })
 
 app.listen(3000,() => {
     console.log("Listen");
