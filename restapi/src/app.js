3e10aaf56a397b900e4d3433e63fdf2b4bf74a57
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-23 18:03:06 +0530

    Use middleware , send data in database

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -6,12 +6,23 @@ require("./db/connection")
 const Student = require("./models/students");
 const PORT = process.env.PORT ||8000;
 
+//! to get data from frontend or Postman we need to use middleware in express js
+app.use(express.json()); //* this is a built-in middlware
+
 app.get('/', (req, res) => {
     res.send('Welcome');
 })
 
 app.post('/students', (req, res) => {
     res.send("Hello from server");
+    console.log(req.body);
+    let user = new Student(req.body);
+    user.save().then(() => {
+        console.log("Data submitted");
+    }).catch((err) => {
+        console.log(err);
+    })
+    
 })
 
 app.listen(PORT,() => {
